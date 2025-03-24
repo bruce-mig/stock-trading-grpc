505acc04a2ec4ba3357ab2a9d87fe944b55a59f9
bruce-mig <brucetmig@icloud.com>
2025-03-24 20:33:28 +0200

    created proto files

---
+++ stock-trading-server/src/test/java/com/github/bruce_mig/stock_trading_server/StockTradingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.stock_trading_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class StockTradingServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
