441d4616031c1da5303a3dcc9744461de186e49e
bruce-mig <brucetmig@icloud.com>
2025-03-24 21:17:31 +0200

    added service implementation

---
+++ stock-trading-server/src/main/java/com/github/bruce_mig/stock_trading_server/service/StockTradingServiceImpl.java
@@ -0,0 +1,35 @@
+package com.github.bruce_mig.stock_trading_server.service;
+
+import com.github.bruce_mig.StockRequest;
+import com.github.bruce_mig.StockResponse;
+import com.github.bruce_mig.StockTradingServiceGrpc;
+import com.github.bruce_mig.stock_trading_server.entity.Stock;
+import com.github.bruce_mig.stock_trading_server.repository.StockRepository;
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
+
+    private final StockRepository stockRepository;
+
+    public StockTradingServiceImpl(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Override
+    public void getStockPrice(StockRequest request, StreamObserver<StockResponse> responseObserver) {
+
+        String stockSymbol = request.getStockSymbol();
+        Stock stockEntity = stockRepository.findByStockSymbol(stockSymbol);
+
+        StockResponse stockResponse = StockResponse.newBuilder()
+                .setStockSymbol(stockEntity.getStockSymbol())
+                .setPrice(stockEntity.getPrice())
+                .setTimestamp(stockEntity.getLastUpdated().toString())
+                .build();
+
+        responseObserver.onNext(stockResponse);
+        responseObserver.onCompleted();
+    }
+}
