b94d0bf7bbaaee60ac395532a9fcd4f9d5ecd072
bruce-mig <brucetmig@icloud.com>
2025-03-24 21:17:46 +0200

    added init sql

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE stocks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  stock_symbol VARCHAR(10) UNIQUE NOT NULL,
+  price DOUBLE PRECISION NOT NULL,
+  last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO stocks (stock_symbol, price) VALUES ('AAPL', 175.50);
+INSERT INTO stocks (stock_symbol, price) VALUES ('GOOGL', 2800.75);
+INSERT INTO stocks (stock_symbol, price) VALUES ('AMZN', 3400.00);
+
+SELECT * FROM stocks s;
+
+ALTER TABLE stocks
+ADD COLUMN currency VARCHAR(3) NOT NULL DEFAULT 'USD';
